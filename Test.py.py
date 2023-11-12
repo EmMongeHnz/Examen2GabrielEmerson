ea9dcf157e9fe3001bdb5ea36c789334c1e3b17b
EMERSON_MONGE_H <mongehernandezemerson@gmail.com>
2023-11-11 19:32:40 -0600

    [Add]: Pruebas Emerson

--- Test.py.py
+++ Test.py.py
@@ -25,6 +25,26 @@ class TestCocinero(unittest.TestCase):
         self.assertEqual(mc.VerificaListaCanciones(
             ["Canción 1", "Canción 2", "Canción 3"]), True)
 
+    def test_ObtieneValencia2(self):
+        mc = MiClase(24736, 116, 15, ["Canción 4", "Canción 5", "Canción 6"], [0.3, 0.6, 0.5])
+        self.assertEqual(mc.ObtieneValencia(24736), 2)
+
+    def test_DivisibleTempo2(self):
+        mc = MiClase(24736, 116, 15, [
+                     "Canción 4", "Canción 5", "Canción 6"], [0.3, 0.6, 0.5])
+        self.assertEqual(mc.DivisibleTempo(116), [1, 2, 4, 29, 58, 116])
+
+    def test_ObtieneMasBailable2(self):
+        mc = MiClase(24736, 116, 15, [
+                     "Canción 4", "Canción 5", "Canción 6"], [0.3, 0.6, 0.5])
+        self.assertEqual(mc.ObtieneMasBailable([0.3, 0.6, 0.5]), 0.6)
+
+    def test_VerificaListaCanciones2(self):
+        mc = MiClase(24736, 116, 15, [
+                     "Canción 4", "Canción 5", "Canción 6"], [0.3, 0.6, 0.5])
+        self.assertEqual(mc.VerificaListaCanciones(
+            ["Canción 4", "Canción 5", "Canción 6"]), True)
+
 
 if __name__ == "__main__":
     unittest.main()
